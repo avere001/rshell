d182ccc6476396eaea7fd8f952786be94364a5b8
avere001 <avere001@ucr.edu>
2015-04-11 20:14:28 -0700

    added prompt and moved line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <sstream>
 
-
 using namespace std;
 
 void toCStrVector(vector<char *> &buffer, vector<string> &original)
@@ -15,20 +14,30 @@ void toCStrVector(vector<char *> &buffer, vector<string> &original)
     buffer.push_back(NULL);
 }
     
-
-int main(int argc, char **argv)
+void printPrompt(ostream &os)
 {
-    vector<string> args;
+    os << "$ ";
+}
+
+void parseLine(vector<string> &args)
+{
     string input = "";
     string line = "";
-    
+
     getline(cin,line);
     stringstream ss(line);
 
     while (ss >> input) {
         args.push_back(input);
     }
+}
 
+int main(int argc, char **argv)
+{
+    vector<string> args;
+  
+    printPrompt(cout); 
+    parseLine(args);
     vector <char *> args_cstr(args.size() + 1);
     toCStrVector(args_cstr, args);  
     
