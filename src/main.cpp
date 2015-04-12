9a9888b8572ea88da5b7c38df5baf8fd1d8a8440
avere001 <avere001@ucr.edu>
2015-04-11 18:03:20 -0700

    removed print statement and did some formatting

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,20 @@ int main(int argc, char **argv)
 {
     string cmd = "";
     vector<char*> args;
-    string input;
+    string input = "";
     string line = "";
     
     getline(cin,line);
     stringstream ss(line);
 
-    int c = 0;
     while (ss >> input) {
 
-        cout << c << endl;
         if (cmd == "")
             cmd = input;
         else
-        {
             args.push_back(&input[0]);
-        }
     }
     args.push_back(NULL);
-    
-    cout << "BEGIN TEST" << endl;
 
     execvp(cmd.c_str(),&args[0]);
     return 0;
