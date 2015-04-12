f7ce8069af6205761c3cd58ff93601e6075afb22
avere001 <avere001@ucr.edu>
2015-04-12 00:52:28 -0700

    added support for ';' connector

--- src/main.cpp
+++ src/main.cpp
@@ -22,34 +22,74 @@ void printPrompt(ostream &os)
     os << "$ ";
 }
 
-void parseLine(vector<string> &args)
+string formatLine(const string &line, const string &delim)
 {
-    string input = "";
-    string line = "";
+    ostringstream ss("");
+    size_t found = 0;
+    size_t prev_found = 0;
+    while ((found = line.find(delim, found+1)) != string::npos)
+    {
+        ss << line.substr(prev_found, found - prev_found) << " " << delim << " ";
+        prev_found = found + 1;
+    }
+    ss << line.substr(prev_found,line.size() -  prev_found);
+    return ss.str();
+}
 
-    getline(cin,line);
-    stringstream ss(line);
-
-    while (ss >> input) {
-        args.push_back(input);
+template<typename T>
+void printVect(vector<T> &v)
+{
+    for (auto &e : v)
+    {
+        cout << e << endl;
     }
 }
 
-void runCommand(vector<string> &args)
+//return is success of parsing
+bool parseLine(vector<vector<string>> &args, vector<string> &connectors)
+{
+    string line = "";
+    getline(cin,line);
+
+    vector<string> curargs;
+    line = formatLine(line, ";");
+
+    stringstream ss(line);
+    string input = "";
+    while (ss >> input) {
+        if (input == ";")
+        {
+            if (curargs.size() == 0)
+            {
+               cerr << "Syntax error near '" << input << "': command must precede connector" << endl;
+               return false;
+            }
+            connectors.push_back(input);
+            args.push_back(curargs);
+            curargs.clear();
+        }
+        else
+        {
+            curargs.push_back(input);
+        }
+    }
+    if (curargs.size() > 0)
+    {
+        args.push_back(curargs);
+    }
+    return true;
+}
+
+int runCommand(vector<string> &args)
 {
     vector <char *> args_cstr(args.size() + 1);
     toCStrVector(args_cstr, args);  
-
 
     int status = 0;
     pid_t pid = fork();
     if (pid == 0)
     {
-        int error = execvp(args_cstr[0],&args_cstr[0]);
-        if (error)
-        {
-            exit(0); //do nothing
-        }
+        return execvp(args_cstr[0],&args_cstr[0]);
     }
     else
     {
@@ -61,11 +101,21 @@ int main(int argc, char **argv)
 {
   
     while (true) {
-        vector<string> args;
+        vector<vector<string>> argsv;
+        vector<string> connectors;
         printPrompt(cout); 
-        parseLine(args);       
-        if (args[0] == "exit") return 0;
-        runCommand(args);
+        if (parseLine(argsv,connectors))
+        {
+            int failure = 0;
+            for (size_t i = 0; i < argsv.size(); ++i) {
+                auto &args = argsv.at(i);
+                if (i == 0 || connectors.at(i-1) == ";")
+                {
+                    if (args.at(0) == "exit") return 0;
+                    runCommand(args);
+                }
+            }
+        }
     }
     return 0;
 }
