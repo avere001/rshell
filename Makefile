045c694ac635914b9834740890ba25b21180af16
Dylan O'Neill <donei003@ucr.edu>
2015-04-29 16:45:43 -0700

    Work cp command ish

--- Makefile
+++ Makefile
@@ -4,3 +4,8 @@ rshell:
 	g++ src/main.cpp -o rshell -Wall -Werror -pedantic --std=c++11
 	mkdir -p bin
 	mv rshell bin/
+cp:
+	g++ src/cp.cpp -o cp -Wall -Werror -pedantic --std=c++11
+	mkdir -p bin
+	mv cp bin/
+	
---
+++ src/cp.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include "timer.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using namespace std;
+int fileDescriptR = 0, fileDescriptW = 0;
+struct stat s;
+ifstream infile;
+ofstream outfile;
+
+void ReadWrite(int argc, char* argv[],int buffsize) {
+    //int n = 1;
+    int n = buffsize;
+    char buffer[BUFSIZ];
+    
+    fileDescriptR = open(argv[1], O_RDONLY);
+    fileDescriptW = open(argv[2], (O_WRONLY | O_CREAT), (S_IWUSR | S_IRUSR));
+    
+
+    ssize_t bytesWritten = 0; 
+    while((bytesWritten = read(fileDescriptR, buffer, n)) != 0) {
+        if(bytesWritten == -1) {
+            perror("Could not write to file");
+            exit(0);
+        }
+        if(write(fileDescriptW, buffer, bytesWritten) == -1) {
+            perror("Could not write to file");
+            exit(0);
+        }
+    }
+    close(fileDescriptR);
+    close(fileDescriptW);
+}
+ 
+ void FileStream() {
+     char c;
+
+    while(infile.get(c)) {
+        outfile.put(c);
+    }
+
+     
+}
+
+int main(int argc, char* argv[]) {
+    Timer t;
+    double wall = 0.0, user = 0.0, system = 0.0;
+
+    if(stat(argv[2],&s) != -1) { 
+        cerr << "The file to be written to already exists" << endl;
+        exit(0);
+    }
+
+    if(fileDescriptR == -1) {
+        perror("Read file could not be opened");
+        exit(0);
+    } 
+    if(fileDescriptW == -1) {
+        perror("Write file could not be opened");
+        exit(0);
+    }
+    infile.open(argv[1]);
+    outfile.open(argv[2]);
+    
+    t.start();
+    FileStream();
+    t.elapsedTime(wall,user,system);
+    cout << "Get/Put Algorithm: "  << endl;
+    cout << "Wall clock: " << wall << endl;
+    cout << "User Time: " << user << endl;
+    cout << "System Time: " << system << endl << endl;
+    
+    t.start();
+    ReadWrite(argc, argv, 1);
+    t.elapsedTime(wall,user,system);
+    cout << "Read/Write Algorithm, Size 1: "  << endl;
+    cout << "Wall clock: " << wall << endl;
+    cout << "User Time: " << user << endl;
+    cout << "System Time: " << system << endl << endl;
+
+    t.start();
+    ReadWrite(argc, argv, BUFSIZ);
+    t.elapsedTime(wall,user,system);
+    cout << "Read/Write Algorithm, Size BUFSIZ: "  << endl;
+    cout << "Wall clock: " << wall << endl;
+    cout << "User Time: " << user << endl;
+    cout << "System Time: " << system << endl;
+
+    infile.close();
+    outfile.close();
+
+    /*Timer t;
+    double eTime;
+    t.start();
+    for (int i=0, j; i<1000000000; i++)
+        j++;
+    t.elapsedUserTime(eTime);
+    cout << eTime << endl;*/
+
+    return 0;
+}
